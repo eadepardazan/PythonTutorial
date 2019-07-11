1de69a016d4b8361e86c8c2945234e9ec4a4b35e
Khoshbin <behdad.khoshbin@gmail.com>
2019-07-12 01:20:19 +0430

    initial Setup

---
+++ hello.py
@@ -0,0 +1,39 @@
+num1 = 3
+num2 = 5.2
+num1 = 4
+x= "Morteza Khoshbin najorsan"
+num1 = (num1 + 2) * 3
+
+num1 += 2
+
+num1 = num1 + 1
+
+num1 +=1
+
+
+if num1 == num2:
+    print("Num1 is equal num2")
+    # print("salam ")
+elif num1 > num2:
+    print("num1 is greater")
+else:
+    print("janevi")
+i=1
+
+# while i<=5:
+#     print("Salam")
+#     i+=1
+# print("End")
+
+
+y = {"apple","cherry","banana"}
+y.add("Orange")
+
+
+for z in y:
+  print(z)
+
+# print(x[3:6])
+
+
+
